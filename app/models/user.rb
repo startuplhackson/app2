c97b1e9d577c138ea90e9aeca68c6e7eda59ea56
StartupLHackson <startuprumors@gmail.com>
2014-04-19 12:29:31 +0100

    add user secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   	uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140419111615) do
+ActiveRecord::Schema.define(:version => 20140419112348) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(:version => 20140419111615) do
     t.integer  "upvotes"
     t.integer  "downvotes"
     t.integer  "totalvotes"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
